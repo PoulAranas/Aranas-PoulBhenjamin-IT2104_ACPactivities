a40de7b7808f5a8bdc8902ee632639753a873e24
PoulAranas <poulbhenjaminaranas@gmail.com>
2024-09-25 11:27:43 +0800

    Delete Laboratory Activity 2 – 25-09-2024 directory

--- Laboratory Activity 2 – 25-09-2024/lab2_item1.py
+++
@@ -1,9 +0,0 @@
-def check_palindrome(number):
-    reversed_number = str(number)[::-1]
-    if str(number) == reversed_number:
-        return f"{number} is a Palindrome"
-    else:
-        return f"{number} is Not a Palindrome"
-
-number = int(input("Enter an integer: "))
-print(check_palindrome(number))
